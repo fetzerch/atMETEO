dd664089508180386f7b9a176e97a06560967dc1
Christian Fetzer <fetzer.ch@gmail.com>
2015-03-01 17:16:37 +0100

    doc: Update generated Doxygen documentation
    Update to commit 0243860f3ca3a05b8abfe1524bc6ec40a1aa2a04.
    Built with Doxygen 1.8.9.1.

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['demodulator',['Demodulator',['../group__libsensors__demodulator.html#singletonSensors_1_1Demodulator',1,'Sensors']]],
+  ['demodulator',['Demodulator',['../group__libsensors__demodulator.html#classSensors_1_1Demodulator',1,'Sensors']]],
   ['demodulator_2eh',['demodulator.h',['../demodulator_8h.html',1,'']]],
   ['demodulatorbase',['DemodulatorBase',['../group__libsensors__demodulator.html#classSensors_1_1DemodulatorBase',1,'Sensors']]],
   ['demodulatorbase_3c_20demodulator_3c_20biphasemark_3c_20tshortmin_2c_20tshortmax_2c_20tlongmin_2c_20tlongmax_20_3e_20_3e_20_3e',['DemodulatorBase&lt; Demodulator&lt; BiphaseMark&lt; TShortMin, TShortMax, TLongMin, TLongMax &gt; &gt; &gt;',['../group__libsensors__demodulator.html',1,'Sensors']]],
---
+++ search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "abcdeghilmnoprstu",
+  1: "bdehlmnorstu",
+  2: "as",
+  3: "bdhimrstu",
+  4: "abcghilmnprstu",
+  5: "bh",
+  6: "bdrs",
+  7: "ciopt",
+  8: "t",
+  9: "abdhimrstu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "groups"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Modules"
+};
+
